567d988512cc0ff0dec2373387aa1e0d78ba350c
Ivan Bachtin <vanobl@mail.ru>
2018-08-28 01:23:59 +0300

    готово задание 5

--- sheet_005.py
+++ sheet_005.py
@@ -2,9 +2,22 @@
 # 32 и заканчивая 127-м включительно. Вывод выполнить в табличной форме: 
 # по десять пар «код-символ» в каждой строке.
 
-spisok = []
+spisok = ''
+chislo = 0
 
 for i in range(78, 128):
-    for j in range(10):
-        spisok[j]
-        #print(f'номер: {i} - символ: {chr(i)}')
+    if chislo < 9:
+        if i < 100:
+            spisok += f' {i}: {chr(i)} '
+            chislo += 1
+        else:
+            spisok += f'{i}: {chr(i)} '
+            chislo += 1
+    else:
+        if i < 100:
+            spisok += f' {i}: {chr(i)}\n'
+            chislo = 0
+        else:
+            spisok += f'{i}: {chr(i)}\n'
+            chislo = 0
+print(spisok)
