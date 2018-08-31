90e604adc97a84326174070bf932d83c4b7e6f4f
Ivan Bochtin <vanobl@mail.ru>
2018-08-31 16:25:16 +0300

    решено четвёртое задание

--- lesson_three_004.py
+++ lesson_three_004.py
@@ -1,12 +1,21 @@
-# Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
-# Количество элементов (n) вводится с клавиатуры.
+#Определить, какое число в массиве встречается чаще всего
 
-a = int(input('Введите целое число: '))
-chislo = 1
-summa = 0
+import random
 
-for i in range(a):
-    summa += chislo
-    chislo /= 2
+repeat = {}
 
-print(summa)
+#определим пустой список
+chisla_list = [1, 2, 3, 5, 6, 5, 2, 5, 2, 1]
+for i in chisla_list:
+    if any(repeat):
+        if chisla_list.count(i) > int(list(repeat.values())[0]):
+            repeat.clear()
+            repeat.update({i: chisla_list.count(i)})
+        elif chisla_list.count(i) == int(list(repeat.values())[0]):
+            repeat.update({i: chisla_list.count(i)})
+    else:
+        repeat.update({i: chisla_list.count(i)})
+
+#выводим результат
+for i, z in repeat.items():
+    print(f'Число: {i} повторяется: {z} раз(а).')
