2955642b3f43cf0667e60c63a487d8b59841731d
Ivan Bochtin <vanobl@mail.ru>
2018-08-31 16:53:35 +0300

    решено шестое задание

--- lesson_three_006.py
+++ lesson_three_006.py
@@ -1,27 +1,53 @@
-#В программе генерируется случайное целое число от 0 до 100. Пользователь 
-# должен его отгадать не более чем за 10 попыток. После каждой неудачной 
-# попытки должно сообщаться, больше или меньше загаданного введенное 
-# пользователем число. Если за 10 попыток число не отгадано, то вывести его.
+#В одномерном массиве найти сумму элементов, находящихся между 
+# минимальным и максимальным элементами. Сами минимальный и 
+# максимальный элементы в сумму не включать.
 
 import random
 
-#генерируем случайное число
-a = random.randint(0, 100)
-#print(a)
+#определим пустой список
+chisla_list = []
 
-popitki = 10
+min_chislo = {}
+max_chislo = {}
 
-while popitki > 0:
-    print(f'Осталось попыток {popitki}')
-    b = int(input('Введите число: '))
-    if b == a:
-        print(f'Вы угадали! Число {a}')
-        break
+#заполним список случайными числами
+for i in range(10):
+    a = random.randint(-100, 100)
+    chisla_list.append(a)
+print(chisla_list)
+
+#узнаем минисмальное число
+for i, x in enumerate(chisla_list):
+    loc_min_chislo = min_chislo.copy()
+    if any(loc_min_chislo):
+        for k, y in loc_min_chislo.items():
+            if x < y:
+                min_chislo.clear()
+                min_chislo.update({i: x})
     else:
-        if b < a:
-            print('Ваше число меньше загаданного.')
-        elif b > a:
-            print('Ваше число больше загаданного')
-    popitki -= 1
+        min_chislo.update({i: x})
 
-print('Программа завершена')
+#узнаём максимальное число
+for i, x in enumerate(chisla_list):
+    loc_max_chislo = max_chislo.copy()
+    if any(loc_max_chislo):
+        for k, y in loc_max_chislo.items():
+            if x > y:
+                max_chislo.clear()
+                max_chislo.update({i: x})
+    else:
+        max_chislo.update({i: x})
+
+print(f'минимальное число: {min_chislo}')
+print(f'максимальное число: {max_chislo}')
+
+summ = 0
+
+if int(list(max_chislo.keys())[0]) < int(list(min_chislo.keys())[0]):
+    for i in range(int(list(max_chislo.keys())[0]) + 1, int(list(min_chislo.keys())[0])):
+        summ += chisla_list[i]
+else:
+    for i in range(int(list(min_chislo.keys())[0]) + 1, int(list(max_chislo.keys())[0])):
+        summ += chisla_list[i]
+
+print(summ)
