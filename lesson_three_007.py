65bd21679ca06992c50b16840735bec43f133347
Ivan Bochtin <vanobl@mail.ru>
2018-08-31 17:08:39 +0300

    решено седьмое

--- lesson_three_007.py
+++ lesson_three_007.py
@@ -1,18 +1,45 @@
-#Написать программу, доказывающую или проверяющую, что для множества 
-# натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2, 
-# где n – любое натуральное число.
+#В одномерном массиве целых чисел определить два наименьших элемента. 
+# Они могут быть как равны между собой (оба являться минимальными), 
+# так и различаться.
 
-n = int(input('Введите число: '))
-a = 0
+import random
 
-b = n * (n + 1) / 2
+#определим пустой список
+chisla_list = []
 
-for i in range(1, n + 1):
-    a += i
-    print(a)
+min_chislo = {}
+max_chislo = {}
 
-if b == a:
-    print(f'{float(a)} = {b}')
-    print('Утверждение верно.')
-else:
-    print('Утверждение не верно.')
+#заполним список случайными числами
+for i in range(10):
+    a = random.randint(-100, 100)
+    chisla_list.append(a)
+print(chisla_list)
+
+#узнаем минисмальное число
+for i, x in enumerate(chisla_list):
+    loc_min_chislo = min_chislo.copy()
+    if any(loc_min_chislo):
+        for k, y in loc_min_chislo.items():
+            if x < y:
+                min_chislo.clear()
+                min_chislo.update({i: x})
+    else:
+        min_chislo.update({i: x})
+
+#узнаём предминимальное число
+for i, x in enumerate(chisla_list):
+    loc_max_chislo = max_chislo.copy()
+    if any(loc_max_chislo):
+        for k, y in loc_max_chislo.items():
+            if int(list(min_chislo.values())[0]) == x and int(list(min_chislo.keys())[0]) == i:
+                pass
+            else:
+                if x < y:
+                    max_chislo.clear()
+                    max_chislo.update({i: x})
+    else:
+        max_chislo.update({i: x})
+
+print(f'минимальное число: {min_chislo}')
+print(f'предминимальное число: {max_chislo}')
