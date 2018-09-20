454533f092d7cf69f6cc5159cb0c8ef1c50d8cde
Ivan Bochtin <vanobl@mail.ru>
2018-09-20 09:19:23 +0300

    выполнено первое задание и подготовлен файл для второго

--- lesson_eight_001.py
+++ lesson_eight_001.py
@@ -3,9 +3,29 @@
 # латинских букв. Требуется найти количество различных подстрок в этой 
 # строке. Для решения задачи рекомендую воспользоваться алгоритмом sha1 
 # из модуля hashlib или встроенной в python функцией hash()
-from binarytree import tree, bst
+import hashlib
 
-a = tree(height=4, is_perfect=False)
+my_str = 'jpmpzwweyg'
+finde_sub = []
+count_hash = 0
 
-b = bst(height=4, is_perfect=True)
-print(b)
+for i in range(1, len(my_str)):
+    for y in range(len(my_str)):
+        sub = my_str[y:y+i]
+        if len(sub) >= i:
+            sub_hash = hashlib.sha1(sub.encode('utf-8')).hexdigest()
+
+            if finde_sub.count(sub_hash) == 0:
+                finde_sub.append(sub_hash)
+                count_hash += 1
+
+print(f'Количество не одинаковых подстрок рyавно {count_hash}.')
+
+
+answer = input('Хотите просмотреть хэш суммы всех подстрок? (y/n): ')
+
+if answer == 'Y':
+    for i in finde_sub:
+        print(i)
+else:
+    print('Вего хорошего!')
